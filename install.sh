4cc5c09dabcb1aac2e8bf5ad2d4a2e88b4a9d8c2
Santiago Perez De Rosso <sperezde@csail.mit.edu>
2013-06-06 18:30:49 -0400

    Install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Creates symlinks to the python files in /usr/bin. After running the install
+# script you can execute the commands by just typing "gl-track", "gl-untrack"
+# and so on in your shell. You only need to execute this script once.
+
+# You'll need to edit this array if you add/remove commands.
+cmds=("track" "untrack")
+
+dir=$(pwd)
+
+for cmd in ${cmds[@]}; do
+  sudo rm "/usr/bin/gl-${cmd}"
+  sudo ln -s "${dir}/gl_${cmd}.py" "/usr/bin/gl-${cmd}"
+done
