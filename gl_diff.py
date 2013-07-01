848b23d351443093bcd967c8c1f5d4c2a0b571f3
Santiago Perez De Rosso <sperezde@csail.mit.edu>
2013-07-01 18:07:03 -0400

    Paged output of diff command

--- gl_diff.py
+++ gl_diff.py
@@ -8,6 +8,9 @@ its version in the repo.
 """
 
 import argparse
+import os
+import subprocess
+import tempfile
 
 import cmd
 import lib
@@ -35,16 +38,21 @@ def main():
           'tracking changes to it with gl track %s' % (fp, fp))
       errors_found = True
     elif ret is lib.SUCCESS:
-      pprint.msg('Diff of file %s with its last committed version' % fp)
+      tf = tempfile.NamedTemporaryFile(delete=False)
+      pprint.msg(
+          'Diff of file %s with its last committed version' % fp, p=tf.write)
       pprint.exp(
           'lines starting with \'-\' are lines that are not in the working '
           'version but that are present in the last committed version of the '
-          'file')
+          'file', p=tf.write)
       pprint.exp(
           'lines starting with \'+\' are lines that are in the working version '
-          'but not in the last committed version of the file')
-      pprint.blank()
-      print out
+          'but not in the last committed version of the file', p=tf.write)
+      pprint.blank(p=tf.write)
+      tf.write(out)
+      tf.close()
+      subprocess.call('less %s' % tf.name, shell=True)
+      os.remove(tf.name)
     else:
       raise Exception('Unrecognized ret code %s' % ret)
 
