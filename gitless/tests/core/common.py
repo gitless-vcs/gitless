6851d3e4198ed4489124f5ca0136cced22483c7e
Santiago Perez De Rosso <sperezde@csail.mit.edu>
2013-10-08 12:14:24 -0400

    First cut at file tests

---
+++ gitless/tests/core/common.py
@@ -0,0 +1,143 @@
+# Gitless - a version control system built on top of Git.
+# Copyright (c) 2013  Santiago Perez De Rosso.
+# Licensed under GNU GPL, version 2.
+
+"""Common methods used in unit tests."""
+
+
+import logging
+import os
+import shutil
+import subprocess
+import sys
+import tempfile
+import unittest
+
+import gitless.core.file as file_lib
+
+
+class TestCore(unittest.TestCase):
+  """Base class for core tests."""
+
+  def setUp(self):
+    """Creates temporary dir and cds to it."""
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    self.path = tempfile.mkdtemp(prefix='gl-core-test')
+    logging.debug('Created temporary directory %s', self.path)
+    os.chdir(self.path)
+
+  def tearDown(self):
+    """Removes the temporary dir."""
+    shutil.rmtree(self.path)
+    logging.debug('Removed dir %s', self.path)
+
+  def _write_file(self, fp, contents='hello'):
+    f = open(fp, 'w')
+    f.write(contents)
+    f.close()
+
+  def _read_file(self, fp):
+    f = open(fp, 'r')
+    ret = f.read()
+    f.close()
+    return ret
+
+  def _git_call(self, subcmd):
+    """Issues a git call with the given subcmd.
+
+    Args:
+      subcmd: e.g., 'add f1'.
+    """
+    logging.debug('Calling git {}'.format(subcmd))
+    p = subprocess.Popen(
+        'git {}'.format(subcmd), stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, shell=True)
+    out, err = p.communicate()
+    if p.returncode != 0:
+      raise Exception('Git call {} failed \nout:{} \nerr:{}'.format(
+        subcmd, out, err))
+
+
+def assert_contents_unchanged(fp):
+  """Decorator that fails the test if the contents of the file fp changed.
+
+  The method decorated should be a unit test.
+
+  Usage:
+    @common.assert_contents_unchanged('f1')
+    def test_method_that_shouldnt_modify_f1(self):
+      # do something here.
+      # assert something here.
+
+  Args:
+    fp: the filepath to assert.
+  """
+  def decorator(f):
+    def wrapper(*args, **kwargs):
+      self = args[0]
+      contents = self._read_file(fp)
+      f(*args, **kwargs)
+      contents_prime = self._read_file(fp)
+      self.assertEqual(
+          contents, contents_prime,
+          'Contents of file changed: from {} to {}'.format(
+               contents, contents_prime))
+    return wrapper
+  return decorator
+
+
+def assert_status_unchanged(fp):
+  """Decorator that fails the test if the status of fp changed.
+
+  The method decorated should be a unit test.
+
+  Usage:
+    @common.assert_status_unchanged('f1')
+    def test_method_that_shouldnt_modify_f1_status(self):
+      # do something here.
+      # assert something here.
+
+  Args:
+    fp: the filepath to assert.
+  """
+  def decorator(f):
+    def wrapper(*args, **kwargs):
+      self = args[0]
+      s = file_lib.status(fp)
+      f(*args, **kwargs)
+      s_prime = file_lib.status(fp)
+      self.assertEqual(
+          s, s_prime,
+          'Status of file changed: from {} to {}'.format(s, s_prime))
+    return wrapper
+  return decorator
+
+
+def assert_no_side_effects(fp):
+  """Decorator that fails the test if the contents or status of fp changed.
+
+  The method decorated should be a unit test.
+
+  Usage:
+    @common.assert_no_side_effects('f1')
+    def test_method_that_shouldnt_affect_f1(self):
+      # do something here.
+      # assert something here.
+
+  It is a shorthand of:
+    @common.assert_status_unchanged('f1')
+    @common.assert_contents_unchanged('f1')
+    def test_method_that_shouldnt_affect_f1(self):
+      # do something here.
+      # assert something here.
+
+  Args:
+    fp: the filepath to assert.
+  """
+  def decorator(f):
+    @assert_contents_unchanged(fp)
+    @assert_status_unchanged(fp)
+    def wrapper(*args, **kwargs):
+      f(*args, **kwargs)
+    return wrapper
+  return decorator
---
+++ gitless/tests/core/test_file.py
@@ -0,0 +1,136 @@
+# Gitless - a version control system built on top of Git.
+# Copyright (c) 2013  Santiago Perez De Rosso.
+# Licensed under GNU GPL, version 2.
+
+"""Unit tests for file module."""
+
+
+import unittest
+
+import gitless.core.file as file_lib
+import common
+
+
+TRACKED_FP = 'f1'
+TRACKED_FP_CONTENTS_1 = 'f1-1'
+TRACKED_FP_CONTENTS_2 = 'f1-2'
+TRACKED_FP_WITH_SPACE = 'f1 space'
+UNTRACKED_FP = 'f2'
+UNTRACKED_FP_WITH_SPACE = 'f2 space'
+IGNORED_FP = 'f3'
+IGNORED_FP_WITH_SPACE = 'f3 space'
+NONEXISTENT_FP = 'nonexistent'
+NONEXISTENT_FP_WITH_SPACE = 'nonexistent'
+
+
+class TestFile(common.TestCore):
+  """Base class for file tests."""
+
+  def setUp(self):
+    super(TestFile, self).setUp()
+
+    # Build up an interesting mock repo.
+    self._git_call('init')
+    self._write_file(TRACKED_FP, contents=TRACKED_FP_CONTENTS_1)
+    self._write_file(TRACKED_FP_WITH_SPACE, contents=TRACKED_FP_CONTENTS_1)
+    self._git_call('add "{}" "{}"'.format(TRACKED_FP, TRACKED_FP_WITH_SPACE))
+    self._git_call(
+        'commit -m"1" "{}" "{}"'.format(TRACKED_FP, TRACKED_FP_WITH_SPACE))
+    self._write_file(TRACKED_FP, contents=TRACKED_FP_CONTENTS_2)
+    self._write_file(TRACKED_FP_WITH_SPACE, contents=TRACKED_FP_CONTENTS_2)
+    self._git_call(
+        'commit -m"2" "{}" "{}"'.format(TRACKED_FP, TRACKED_FP_WITH_SPACE))
+    self._write_file(UNTRACKED_FP)
+    self._write_file(UNTRACKED_FP_WITH_SPACE)
+    self._write_file(
+      '.gitignore', contents='{}\n{}'.format(IGNORED_FP, IGNORED_FP_WITH_SPACE))
+    self._write_file(IGNORED_FP)
+    self._write_file(IGNORED_FP_WITH_SPACE)
+
+
+class TestTrackFile(TestFile):
+
+  @common.assert_contents_unchanged(UNTRACKED_FP)
+  def test_track_untracked_fp(self):
+    self.assertEqual(file_lib.SUCCESS, file_lib.track(UNTRACKED_FP))
+    self.assertEqual(file_lib.TRACKED, file_lib.status(UNTRACKED_FP).type)
+
+  @common.assert_contents_unchanged(UNTRACKED_FP_WITH_SPACE)
+  def test_track_untracked_fp_with_space(self):
+    self.assertEqual(file_lib.SUCCESS, file_lib.track(UNTRACKED_FP_WITH_SPACE))
+    self.assertEqual(
+      file_lib.TRACKED, file_lib.status(UNTRACKED_FP_WITH_SPACE).type)
+
+  @common.assert_no_side_effects(TRACKED_FP)
+  def test_track_tracked_fp(self):
+    self.assertEqual(file_lib.FILE_ALREADY_TRACKED, file_lib.track(TRACKED_FP))
+
+  @common.assert_no_side_effects(TRACKED_FP_WITH_SPACE)
+  def test_track_tracked_fp_with_space(self):
+    self.assertEqual(
+      file_lib.FILE_ALREADY_TRACKED, file_lib.track(TRACKED_FP_WITH_SPACE))
+
+  def test_track_nonexistent_fp(self):
+    self.assertEqual(file_lib.FILE_NOT_FOUND, file_lib.track(NONEXISTENT_FP))
+
+  def test_track_nonexistent_fp_with_space(self):
+    self.assertEqual(
+      file_lib.FILE_NOT_FOUND, file_lib.track(NONEXISTENT_FP_WITH_SPACE))
+
+  @common.assert_no_side_effects(IGNORED_FP)
+  def test_track_ignored(self):
+    self.assertEqual(file_lib.FILE_IS_IGNORED, file_lib.track(IGNORED_FP))
+
+  @common.assert_no_side_effects(IGNORED_FP_WITH_SPACE)
+  def test_track_ignored_with_space(self):
+    self.assertEqual(
+      file_lib.FILE_IS_IGNORED, file_lib.track(IGNORED_FP_WITH_SPACE))
+
+
+class TestUntrackFile(TestFile):
+
+  @common.assert_contents_unchanged(TRACKED_FP)
+  def test_untrack_tracked_fp(self):
+    self.assertEqual(file_lib.SUCCESS, file_lib.untrack(TRACKED_FP))
+    self.assertEqual(file_lib.UNTRACKED, file_lib.status(TRACKED_FP).type)
+
+  @common.assert_no_side_effects(UNTRACKED_FP)
+  def test_untrack_untracked_fp(self):
+    self.assertEqual(
+        file_lib.FILE_ALREADY_UNTRACKED, file_lib.untrack(UNTRACKED_FP))
+
+  def test_untrack_nonexistent_fp(self):
+    self.assertEqual(file_lib.FILE_NOT_FOUND, file_lib.untrack(NONEXISTENT_FP))
+
+  @common.assert_no_side_effects(IGNORED_FP)
+  def test_untrack_ignored(self):
+    self.assertEqual(file_lib.FILE_IS_IGNORED, file_lib.untrack(IGNORED_FP))
+
+
+class TestCheckoutFile(TestFile):
+
+  @common.assert_no_side_effects(TRACKED_FP)
+  def test_checkout_fp_at_head(self):
+    contents = self._read_file(TRACKED_FP)
+    self._write_file(TRACKED_FP, contents='contents')
+    self.assertEqual(file_lib.SUCCESS, file_lib.checkout(TRACKED_FP)[0])
+    self.assertEqual(contents, self._read_file(TRACKED_FP))
+
+  def test_checkout_fp_at_cp_other_than_head(self):
+    self._write_file(TRACKED_FP, contents='contents')
+    self.assertEqual(
+        file_lib.SUCCESS, file_lib.checkout(TRACKED_FP, 'HEAD^1')[0])
+    self.assertEqual(TRACKED_FP_CONTENTS_1, self._read_file(TRACKED_FP))
+
+  @common.assert_no_side_effects(UNTRACKED_FP)
+  def test_checkout_uncommited_fp(self):
+    self.assertEqual(
+        file_lib.FILE_NOT_FOUND_AT_CP, file_lib.checkout(UNTRACKED_FP)[0])
+
+  def test_checkout_nonexistent_fp(self):
+    self.assertEqual(
+        file_lib.FILE_NOT_FOUND_AT_CP, file_lib.checkout(NONEXISTENT_FP)[0])
+
+
+if __name__ == '__main__':
+  unittest.main()
