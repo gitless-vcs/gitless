b37ea5cca0a05377d9e43b76f457c0e27e36ce6a
Santiago Perez De Rosso <sperezde@csail.mit.edu>
2013-11-23 13:17:21 -0500

    Release 0.4

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 
 setup(
     name='gitless',
-    version='0.3',
+    version='0.4',
     description='A version control system built on top of Git',
     long_description=open('README.md').read(),
     author='Santiago Perez De Rosso',
     author_email='sperezde@csail.mit.edu',
     url='http://github.com/spderosso/gitless',
     packages=['gitless'],
-    install_requires=['gitpylib>=0.2'],
+    install_requires=['gitpylib>=0.4'],
     license='GPLv2',
     classifiers=(
       'Development Status :: 2 - Pre-Alpha',
