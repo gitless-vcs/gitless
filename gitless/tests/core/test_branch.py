4b6042df181ca14eacf50bfe28d71f6112558a07
Santiago Perez De Rosso <sperezde@csail.mit.edu>
2013-12-16 16:03:26 -0500

    branch switch tests

--- gitless/tests/core/test_branch.py
+++ gitless/tests/core/test_branch.py
@@ -8,6 +8,7 @@
 import os
 import unittest
 
+import gitless.core.file as file_lib
 import gitless.core.branch as branch_lib
 import common
 
@@ -16,7 +17,9 @@ TRACKED_FP = 'f1'
 TRACKED_FP_CONTENTS_1 = 'f1-1'
 TRACKED_FP_CONTENTS_2 = 'f1-2'
 UNTRACKED_FP = 'f2'
+UNTRACKED_FP_CONTENTS = 'f2'
 IGNORED_FP = 'f3'
+BRANCH = 'b1'
 
 
 class TestBranch(common.TestCore):
@@ -32,9 +35,10 @@ class TestBranch(common.TestCore):
     self._git_call('commit -m"1" "{}"'.format(TRACKED_FP))
     self._write_file(TRACKED_FP, contents=TRACKED_FP_CONTENTS_2)
     self._git_call('commit -m"2" "{}"'.format(TRACKED_FP))
-    self._write_file(UNTRACKED_FP)
+    self._write_file(UNTRACKED_FP, contents=UNTRACKED_FP_CONTENTS)
     self._write_file('.gitignore', contents='{}'.format(IGNORED_FP))
     self._write_file(IGNORED_FP)
+    self._git_call('branch "{}"'.format(BRANCH))
 
 
 class TestCreate(TestBranch):
@@ -76,6 +80,44 @@ class TestDelete(TestBranch):
   def test_delete_nonexistent_branch(self):
     self.assertEqual(
         branch_lib.NONEXISTENT_BRANCH, branch_lib.delete('nonexistent'))
+
+  def test_delete(self):
+    self.assertEqual(
+        branch_lib.SUCCESS, branch_lib.delete(BRANCH))
+
+
+class TestSwitch(TestBranch):
+
+  def test_switch_contents_still_there_untrack_tracked(self):
+    file_lib.untrack(TRACKED_FP)
+    self._write_file(TRACKED_FP, contents='contents')
+    self.assertEqual(branch_lib.SUCCESS, branch_lib.switch(BRANCH))
+    self.assertEqual(TRACKED_FP_CONTENTS_2, self._read_file(TRACKED_FP))
+    self.assertEqual(branch_lib.SUCCESS, branch_lib.switch('master'))
+    self.assertEqual('contents', self._read_file(TRACKED_FP))
+
+  def test_switch_contents_still_there_untracked(self):
+    self.assertEqual(branch_lib.SUCCESS, branch_lib.switch(BRANCH))
+    self._write_file(UNTRACKED_FP, contents='contents')
+    self.assertEqual(branch_lib.SUCCESS, branch_lib.switch('master'))
+    self.assertEqual(UNTRACKED_FP_CONTENTS, self._read_file(UNTRACKED_FP))
+    self.assertEqual(branch_lib.SUCCESS, branch_lib.switch(BRANCH))
+    self.assertEqual('contents', self._read_file(UNTRACKED_FP))
+
+  def test_switch_contents_still_there_tracked_commit(self):
+    self._write_file(TRACKED_FP, contents='commit')
+    self._git_call('commit -m\'comment\' {}'.format(TRACKED_FP))
+    self.assertEqual(branch_lib.SUCCESS, branch_lib.switch(BRANCH))
+    self.assertEqual(TRACKED_FP_CONTENTS_2, self._read_file(TRACKED_FP))
+    self.assertEqual(branch_lib.SUCCESS, branch_lib.switch('master'))
+    self.assertEqual('commit', self._read_file(TRACKED_FP))
+
+  def test_switch_file_classification_is_mantained(self):
+    file_lib.untrack(TRACKED_FP)
+    self.assertEqual(branch_lib.SUCCESS, branch_lib.switch(BRANCH))
+    self.assertEqual(file_lib.TRACKED, file_lib.status(TRACKED_FP).type)
+    self.assertEqual(branch_lib.SUCCESS, branch_lib.switch('master'))
+    self.assertEqual(file_lib.UNTRACKED, file_lib.status(TRACKED_FP).type)
 
 
 if __name__ == '__main__':
