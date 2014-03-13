6061733d40297ebb90e7c9c022ac6eab064f9f3b
Santiago Perez De Rosso <sperezde@csail.mit.edu>
2014-03-13 16:18:57 -0400

    cleanup in gl diff

--- gitless/cli/gl_diff.py
+++ gitless/cli/gl_diff.py
@@ -65,14 +65,14 @@ def main(args):
             'committed version. No diffs to output'.format(fp))
         continue
 
-      tf = tempfile.NamedTemporaryFile(mode='w', delete=False)
-      pprint.msg(
-          'Diff of file {0} with its last committed version'.format(fp),
-          p=tf.write)
-      pprint.msg('{0} lines added'.format(additions), p=tf.write)
-      pprint.msg('{0} lines removed'.format(deletions), p=tf.write)
-      pprint.diff(out, padding, p=tf.write)
-      tf.close()
+      with tempfile.NamedTemporaryFile(mode='w', delete=False) as tf:
+        pprint.msg(
+            'Diff of file {0} with its last committed version'.format(fp),
+            p=tf.write)
+        pprint.msg('{0} lines added'.format(additions), p=tf.write)
+        pprint.msg('{0} lines removed'.format(deletions), p=tf.write)
+        pprint.diff(out, padding, p=tf.write)
+
       subprocess.call('less -r -f {0}'.format(tf.name), shell=True)
       os.remove(tf.name)
     else:
