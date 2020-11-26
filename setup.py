6b85540e57afb8780d8596a615b4e534fa1e691a
Rui Chen <rui@meetup.com>
2020-11-26 16:43:38 -0500

    deps: update libgit2 to 1.4.0
    previous update, #230
    
    relates to https://github.com/Homebrew/homebrew-core/pull/65706
    
    Signed-off-by: Rui Chen <rui@meetup.com>

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     packages=['gitless', 'gitless.cli'],
     install_requires=[
       # make sure install_requires is consistent with requirements.txt
-      'pygit2==1.2.0', # requires libgit2 1.0.x
+      'pygit2==1.4.0', # requires libgit2 1.1.x
       'argcomplete>=1.11.1'
     ],
     license='MIT',
