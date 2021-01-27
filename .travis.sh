982e47a65cf62682d1bb53eb7b30cda8b772f50e
Santiago Perez De Rosso <spderosso@gmail.com>
2021-01-26 22:53:22 -0800

    Update travis.sh

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
+# Based on pygit2's .travis.sh
 
+PREFIX=/home/travis/install
+
+# Build libssh2 1.9.0 (Ubuntu only has 1.8.0, which doesn't work)
 cd ~
+wget https://www.libssh2.org/download/libssh2-1.9.0.tar.gz
+tar xf libssh2-1.9.0.tar.gz
+cd libssh2-1.9.0
+./configure --prefix=/usr --disable-static && make
+sudo make install
 
-git clone --depth=1 -b ethomson/v1.1 https://github.com/libgit2/libgit2.git
+# Build libgit2
+cd ~
+git clone --depth=1 -b "maint/v1.1" https://github.com/libgit2/libgit2.git
 cd libgit2/
 
 mkdir build && cd build
